539ab82893d980720e53cf4eb0aefe768b0d53a7
Xandy77 <domirops@yahoo.com.br>
2024-08-23 17:03:27 -0300

    versão 1.0

---
+++ main.py
@@ -0,0 +1,12 @@
+# lambda
+calcular_pg = lambda x: x*2
+
+# Progresão Geometrica
+numeros =[1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# usando a função map para gerar uma lista de progressão geométrica
+pg = list(map(calcular_pg, numeros))
+
+# exibe na tela a lista
+for i in pg:
+    print(i)
